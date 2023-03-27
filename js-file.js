6e849af76f4bbcb23c55d5318c49c4d34555c2af
Oscar Marulanda <odmarulandab@unal.edu.co>
2023-03-26 23:32:05 -0500

    fix calculator functions. can now use all buttons well without errors

--- js-file.js
+++ js-file.js
@@ -3,44 +3,64 @@ const display = document.querySelector('#display');
 const val = document.querySelectorAll('.grid-item');
 const operand = document.querySelectorAll('.grid-item-cal');
 const equals = document.querySelector('#igual');
+const borrar = document.querySelector('#del');
 
-let operation = [];
-let value1;
-let value2;
+let values = [];
+let displayy = [];
+
 let sign;
 
 val.forEach((va) => {
     va.addEventListener('click', () => {
-    display.textContent+= +va.innerText;
-    operation.push(va.innerText);
+    displayy.push(va.innerText);
+    display.textContent = displayy.join('');
+    values.push(va.innerText);
 });
 });
 
 operand.forEach((op) => {
     op.addEventListener('click', () => {
-    display.textContent+=op.innerText;
-    value1 = operation.join('');
-    operation = [];
-    sign = op.innerText;
+    if(values[0]==undefined && op.id=="-"){
+        displayy.push(op.innerText);
+        display.textContent = displayy.join('');
+        values.push(op.id);
+    }
+    if(values[0]==undefined){
+        return;
+    }else if(values[values.length-1]=='+'||values[values.length-1]=='-'||values[values.length-1]=='*'||values[values.length-1]=='/'||values[values.length-1]=='**'){
+        values[values.length-1]=op.id;
+        displayy[displayy.length - 1]=op.innerText;
+        display.textContent = displayy.join('');
+    }else{
+        displayy.push(op.innerText);
+        display.textContent = displayy.join('');
+        values.push(op.id);
+    };
 });
 });
 
 ac.addEventListener('click', () => {
     display.textContent=' ';
-    operation = [];
+    values = [];
+    displayy = [];
 })
 
 equals.addEventListener('click', () => {
-    if(sign=='+'){
-        display.textContent = +value1 + +operation.join('');
-    } else if(sign=='x'){
-        display.textContent = +value1 * +operation.join('');
-    } else if(sign=='-'){
-        display.textContent = +value1 - +operation.join('');
-    } else if(sign=='÷'){
-        display.textContent = +value1 / +operation.join('');
+    
+    function evil(fn) {
+        return new Function('return ' + fn)();
+      }
+      
+    if(evil(values.join(''))=="Infinity"){
+        display.textContent=":v"
+    }else{
+        display.textContent = evil(values.join(''));
     }
-
     
 });
 
+borrar.addEventListener('click', () => {
+    displayy = displayy.slice(0, -1);
+    values = values.slice(0, -1);
+    display.textContent = displayy.join('');
+});
